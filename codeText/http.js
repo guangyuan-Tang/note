1b0d4fe3c2071cb85ba859ab5f62791db5f96c1a
光源 <guangyuan_y@163.com>
2019-07-30 00:20:42 +0800

    http修改

--- codeText/http.js
+++ codeText/http.js
@@ -1,81 +1,134 @@
-import axios from "axios"
-import $url from './url'
-// import $public from './utils/public.js'
-
+import axios from "axios";
+import $url from "./url";
+import $util from "./util";
+import $store from "@/store/";
 
 const TIMEOUT = 5000; //请求超时延迟
 
+//新建一个axios实例  用该实例发起请求并设置拦截器，避免影响全局的axios
+var instance = axios.create();
 
-// 添加请求拦截器 
-axios.interceptors.request.use(config => {
-  //默认开启loading
-  if (!config.hideLoading) {
-    // loading
+// 添加请求拦截器
+axios.interceptors.request.use(
+  config => {
+    //兼容不需要loading的请求 (参数内带hideLoading: true)
+    let params = {};
+    let copyConfig = $util.deepClone(config);
+    switch (copyConfig.method) {
+      case "get":
+      case "del":
+        params = copyConfig.params;
+        break;
+      case "put":
+      case "post":
+      case "patch":
+        params = copyConfig.data;
+        break;
+      default:
+        params = copyConfig.params;
+        break;
+    }
+
+    //默认开启loading
+    if (!params.hideLoading) {
+      $store.commit("showLoading");
+    }
+
+    return config;
+  },
+  error => {
+    $store.commit("hideLoading");
+    return Promise.reject(error);
   }
-  return config
-}, error => {
-  //关闭loading
-    return Promise.reject(error)
-});
+);
 
 // 添加响应拦截器
-axios.interceptors.response.use(response => {  
-  // 响应成功关闭loading
-  return response
-}, error => {
-  // 对响应错误做点什么
-  return Promise.resolve(error);
-
-});
-
-
-
-async get(url, data) {
-  return await this.request("GET", url, data);
-}
-
-async post(url, data) {
-  return await this.request("POST", url, data);
-}
-
+axios.interceptors.response.use(
+  response => {
+    $store.commit("hideLoading");
+    return response;
+  },
+  error => {
+    $store.commit("hideLoading");
+    return Promise.reject(error.response || {});
+  }
+);
 
 /**
-* 异步请求
-* @param {String} method 请求方式
-* @param {String} url 请求地址
-* @param {Object} data 请求数据
-*
-* @returns {Promise}
-*/
+ * 异步请求
+ * @param {String} method 请求方式
+ * @param {String} url 请求地址
+ * @param {Object} data 请求数据
+ *
+ * @returns {Promise}
+ */
 
-request(method, url, params) {
-
+function request(method, url, _params) {
   //参数处理
-  params['_'] = Math.random();
-  let ajaxUrl = $url.request[_url] || url
+  let params = _params || {};
+  let ajaxUrl = $url[url] || url;
+  params["_"] = Math.random();
 
   let ajaxParams = {
     url: ajaxUrl,
     method: method,
     timeout: TIMEOUT,
     headers: {
-      'access-token':$public.getStorage('USER_ACCESS_TOKEN'),
-      'Content-Type': 'application/json',
-    },
+      Authorization: "Bearer" + " " + $util.getLocal("access_token"),
+      "X-Requested-With": "XMLHttpRequest"
+    }
+  };
+
+  switch(method) {
+    case "get":
+    case "del":
+      ajaxParams.params = params;
+      break;
+    case "put":
+    case "post":
+    case "patch":
+      ajaxParams.data = params
+      break;
+    default:
+      ajaxParams.params = params;
+      break;
   }
 
-  method == 'post' ? ajaxParams.data = params || {} : ajaxParams.params = params || {}
-
   return new Promise((resolve, reject) => {
-    axios(ajaxParams).then(res => {
-        resolve(res)
-    }).catch(error => {
-        reject(error)
-    });
-  })
+    axios(ajaxParams)
+      .then(res => {
+        resolve(res.data);
+      })
+      .catch(error => {
+        reject(error.data);
+      });
+  });
 }
 
+function get(url, data) {
+  return request("get", url, data);
+}
+
+function post(url, data) {
+  return request("post", url, data);
+}
+
+function put(url, data) {
+  return request("put", url, data);
+}
+
+function patch(url, data) {
+  return request("patch", url, data);
+}
+
+function del(url, data) {
+  return request("delete", url, data);
+}
 
 export default {
-  get, post
-}
+  get,
+  post,
+  put,
+  patch,
+  del
+};
